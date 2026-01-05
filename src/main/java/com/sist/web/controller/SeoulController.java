cbf186b3a68243424a71c3367291b3be8037a1e0
jieunpyo <jieunhye1121@naver.com>
2026-01-05 16:32:18 +0900

    1111

--- src/main/java/com/sist/web/controller/SeoulController.java
+++ src/main/java/com/sist/web/controller/SeoulController.java
@@ -106,7 +106,8 @@ public class SeoulController {
 	  String jsp="";
 	  if(contenttype==12)
 	  {
-		  
+		  SeoulVO vo=sService.seoulAttractionDetailData(contentid);
+		  model.addAttribute("vo", vo);
 		  jsp="../seoul/attraction.jsp";
 	  }
 	  else if(contenttype==14)
